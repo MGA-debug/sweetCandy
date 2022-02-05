bc8904cd2ef0ad10256b72c34dee47c512ee7ef4
Misha <gordeev-mihaill@yandex.ru>
2022-02-05 13:47:40 +0300

    Поменял список на массив

--- src/ru/appline/logic/sweets/SweetGift.java
+++ src/ru/appline/logic/sweets/SweetGift.java
@@ -1,28 +1,23 @@
 package ru.appline.logic.sweets;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.util.Arrays;
 
 public class SweetGift {
 
-    private List<Candy> sweetBox = new ArrayList<>();
+    private Candy[] sweetBox;
 
     public SweetGift collectGift(Candy... candies) {
-        sweetBox = Stream
-                .of(candies)
-                .collect(Collectors.toList());
+        sweetBox = candies;
         return this;
     }
 
     public void getInformation() {
         System.out.println("Подарок содержит: ");
-        sweetBox.forEach(System.out::println);
-        System.out.println("Общий вес: " + sweetBox.stream()
+        Arrays.stream(sweetBox).forEach(System.out::println);
+        System.out.println("Общий вес: " + Arrays.stream(sweetBox)
                 .map(Candy::getWeight)
                 .reduce(0, Integer::sum));
-        System.out.println("Общая стоимость: " + sweetBox.stream()
+        System.out.println("Общая стоимость: " + Arrays.stream(sweetBox)
                 .map(Candy::getCost)
                 .reduce(0.0, Double::sum));
     }
